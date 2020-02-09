9e4f10310b19a5f822cc8c123807071b89f631df
Marcial Paul Gargoles <im.codename@gmail.com>
2020-02-10 06:27:49 +0800

    shorter code

--- profile-safeguard.js
+++ profile-safeguard.js
@@ -1,34 +1,5 @@
 // author: https://github.com/marcialpaulg
 
-var jazoest = document.querySelector('input[name="jazoest"]').value,
-    fb_dtsg = document.querySelector('input[name="fb_dtsg"]').value,
-    session_id = document.querySelector('input[name="composer_session_id"]').value,
-    user_id = document.cookie.split('c_user=')[1].split(';')[0];
-    
-var param = {
-    av: user_id,
-    __user: user_id,
-    __a: '1',
-    __dyn: '',
-    __csr: '',
-    __req: 'e',
-    __beoa: '0',
-    __pc: 'PHASED:DEFAULT',
-    dpr: '1',
-    __rev: '1001692495',
-    __s: '',
-    __hsi: '',
-    fb_dtsg: fb_dtsg,
-    jazoest: jazoest,
-    __spin_r: '1001692495',
-    __spin_b: 'trunk',
-    __spin_t: (new Date().getTime()),
-    fb_api_caller_class: 'IsShieldedSetMutation',
-    fb_api_req_friendly_name: 'IsShieldedSetMutation',
-    variables: '{\"0\":{\"is_shielded\":true,\"session_id\":\"'+session_id+'\",\"actor_id\":\"'+user_id+'\",\"client_mutation_id\":\"'+session_id+'\"}}',
-    doc_id: '1477043292367183',
-};
-
 fetch("https://www.facebook.com/api/graphql/", {
     "credentials": "include",
     "headers": {
@@ -41,9 +12,33 @@ fetch("https://www.facebook.com/api/graphql/", {
     },
     "referrer": "https://www.facebook.com/settings",
     "referrerPolicy": "origin-when-cross-origin",
-    "body": Object.keys(param).map(function(key) {
+    "body": (function(param){
+        return Object.keys(param).map(function(key) {
             return key + '=' + encodeURIComponent(param[key]);
-        }).join('&'),
+        }).join('&')
+    })({
+        av: (user_id = document.cookie.split('c_user=')[1].split(';')[0]),
+        __user: user_id,
+        __a: '1',
+        __dyn: '',
+        __csr: '',
+        __req: 'e',
+        __beoa: '0',
+        __pc: 'PHASED:DEFAULT',
+        dpr: '1',
+        __rev: '1001692495',
+        __s: '',
+        __hsi: '',
+        fb_dtsg: document.querySelector('input[name="fb_dtsg"]').value,
+        jazoest: document.querySelector('input[name="jazoest"]').value,
+        __spin_r: '1001692495',
+        __spin_b: 'trunk',
+        __spin_t: (new Date().getTime()),
+        fb_api_caller_class: 'IsShieldedSetMutation',
+        fb_api_req_friendly_name: 'IsShieldedSetMutation',
+        variables: '{\"0\":{\"is_shielded\":true,\"session_id\":\"'+(session_id = document.querySelector('input[name="composer_session_id"]').value)+'\",\"actor_id\":\"'+user_id+'\",\"client_mutation_id\":\"'+session_id+'\"}}',
+        doc_id: '1477043292367183',
+    }),
     "method": "POST",
     "mode": "cors"
 });
